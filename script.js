eaf19a361bc5a186753ee27173e6c20259ef24e1
QRCode-AB83JQC3ZSPP92H14BYJQ <iriax72@gmail.com>
2026-05-10 19:59:33 +0000

    meilleur timeout avant printAlert

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ const counterBar = counter.querySelector("#counter-bar-int");
 const basicSleepTime = 1250;
 const nbrOfIncrase = 5;
 
-function chargeData () {
-    for (let i = 0; i < nbrOfIncrase; i++) {
-        setTimeout(incrase, basicSleepTime * (i + 1));
-    }
-}
-
 function incrase () {
     let value = parseInt(counterBar.dataset.value) || 0;
     value++;
@@ -19,16 +13,20 @@ function incrase () {
 
 function printAlert () {
     const text = "";
+
     if (confirm("Votre appareil a été priaté. Voulez-vous activer la protection anti-virus ?")) {
         text = "Haha, j'ai réussi à vous faure appuyer sur le bouton !\nVous avez de la chance que je ne sois pas un vrai pkrate informatique, sans quoi toutes vos données auraient déjà été volées !\nLa prochaine fois, réfléchissez à deux fois avant de scanner un QRCode inconnu.";
     } else {
         text = "Vous n'avez pas cliqué ? Me voilà étonné.\nDams tous les cas, sachez que votre appareil n'a pas été piraté.\nCependant, j'ai tout de même réussi à vous envoyer sur ce site web sans que vous n'ayez votre mot à dire. La prochaine fois, réfléchissez à deux fois avant de scanner un QR Code inconnu !";
     }
+    
     const p = body.createElement("p");
     p.innerText = text;
     body.appendChild(p);
 }
 
-chargeData();
+for (let i = 0 ; i < nbrOfIncrase ; i++) {
+    setTimeout(incrase, basicSleepTime * (i + 1))
+};
 
 setTimeout(printAlert, 2009);
