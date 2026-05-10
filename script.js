83a5a86c33b110cf7a21478be4c57b85964e6194
QRCode-AB83JQC3ZSPP92H14BYJQ <iriax72@gmail.com>
2026-05-10 19:23:13 +0000

    css

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const counter = document.querySelector("#counter-bar-ext");
 const counterBar = counter.querySelector("#counter-bar-int");
 
+const basicSleepTime = 1250;
+const nbrOfIncrase = 5;
+
 function chargeData () {
-    for (let i = 0; i <= 5; i++) {
-        setTimeout(incrase, 750 * (i + 1));
+    for (let i = 0; i < nbrOfIncrase; i++) {
+        setTimeout(incrase, basicSleepTime * (i + 1));
     }
 }
 
@@ -11,7 +14,7 @@ function incrase () {
     let value = parseInt(counterBar.dataset.value) || 0;
     value++;
     counterBar.dataset.value = value;
-    counterBar.style.width = (value * 100) + "px";
+    counterBar.style.width = (value * 350 / nbrOfIncrase) + "px";
 }
 
 chargeData();
