5d3282efcd91cc672397d4a127f5c182174bd243
QRCode-AB83JQC3ZSPP92H14BYJQ <iriax72@gmail.com>
2026-05-10 20:12:28 +0000

    finalisation

--- script.js
+++ script.js
@@ -21,12 +21,15 @@ const runIncrements = async () => {
 };
 
 function printAlert () {
+    document.querySelector("#counter-text").remove();
+    document.querySelector("#counter-bar-ext").remove();
+
     let text = "";
 
-    if (confirm("Votre appareil a été priaté. Voulez-vous activer la protection anti-virus ?")) {
-        text = "Haha, j'ai réussi à vous faure appuyer sur le bouton !\nVous avez de la chance que je ne sois pas un vrai pkrate informatique, sans quoi toutes vos données auraient déjà été volées !\nLa prochaine fois, réfléchissez à deux fois avant de scanner un QRCode inconnu.";
+    if (confirm("Votre appareil a été piraté. Voulez-vous activer la protection anti-virus ?")) {
+        text = "Haha, j'ai réussi à vous faire appuyer sur le bouton !\n\nVous avez de la chance que je ne sois pas un vrai pirate informatique, sans quoi toutes vos données auraient déjà été volées !\n\nLa prochaine fois, réfléchissez à deux fois avant de scanner un QR Code inconnu.";
     } else {
-        text = "Vous n'avez pas cliqué ? Me voilà étonné.\nDams tous les cas, sachez que votre appareil n'a pas été piraté.\nCependant, j'ai tout de même réussi à vous envoyer sur ce site web sans que vous n'ayez votre mot à dire. La prochaine fois, réfléchissez à deux fois avant de scanner un QR Code inconnu !";
+        text = "Vous n'avez pas cliqué ? Me voilà étonné.\n\nDans tous les cas, sachez que votre appareil n'a pas été piraté.\n\nCependant, j'ai tout de même réussi à vous envoyer sur ce site web sans que vous n'ayez eu votre mot à dire. La prochaine fois, réfléchissez à deux fois avant de scanner un QR Code inconnu !";
     }
 
     const p = document.createElement("p");
