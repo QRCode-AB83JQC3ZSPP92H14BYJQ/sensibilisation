d4cc73128adac681191b333550ea592877eeb499
QRCode-AB83JQC3ZSPP92H14BYJQ <iriax72@gmail.com>
2026-05-10 19:15:22 +0000

    patch

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const counter = document.querySelector("counter-bar-ext");
+const counter = document.querySelector("#counter-bar-ext");
+const counterBar = counter.querySelector("#counter-bar-int");
 
 function chargeData () {
-    for (i=0 ; i <= 5 ; i++) {
-        setTimeout(incrase, 750);
+    for (let i = 0; i <= 5; i++) {
+        setTimeout(incrase, 750 * (i + 1));
     }
 }
 
 function incrase () {
-    [...counter.children][0].dataset.value ++;
+    let value = parseInt(counterBar.dataset.value) || 0;
+    value++;
+    counterBar.dataset.value = value;
+    counterBar.style.width = (value * 100) + "px";
 }
+
+chargeData();
